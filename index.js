9c78528370b48e13fccbbc29614223a3bb9acac2
alimancs <aliman2952003@gmail.com>
2024-09-30 13:55:44 +0100

    commit to handle login error 5

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/login', async (request, response) => {
 // handle token verification 
 app.get( '/profile', async (request, response ) => {
     let user ;
-    const { token } = request.cookies;
+    const { token } = request.cookies.token;
     const isverified = jwt.verify( token, secretpk, {}, ( error, decoded ) => {
         if (error) throw error ;
         user = decoded ;
@@ -155,7 +155,7 @@ app.put('/post', uploadMiddleWare.single('file'), async ( request, response) => 
     })
 })
 
-app.listen('https://paperplane-blog-api.onrender.com');
+app.listen(4000);
 
 
 

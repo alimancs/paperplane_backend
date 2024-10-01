57e9a2275b86d6dc2ccccae42f54f37e827ae0f7
alimancs <aliman2952003@gmail.com>
2024-10-01 18:40:20 +0100

    latest commit

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post('/login', async (request, response) => {
 
 // handle token verification 
 app.get( '/profile', (request, response ) => {
-    const token = request.headers.cookie;
+    const token = request.headers.authorization;
     jwt.verify( token, secretpk, {}, ( error, decoded ) => {
         if (error) throw error ;
         response.json(decoded) ;

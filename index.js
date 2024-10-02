4de1d9ff21fae8524c8a565fc3758cee82cf89a5
alimancs <aliman2952003@gmail.com>
2024-10-02 14:13:26 +0100

    latest commit

--- index.js
+++ index.js
@@ -64,16 +64,24 @@ app.post('/login', async (request, response) => {
 
 // handle token verification 
 app.get( '/profile', (request, response ) => {
-    const token = request.headers.authorization;
-    // response.setHeader('Access-Control-Allow-Origin', 'https://paperplane-blog.onrender.com');
-    // if ( token === '' ) {
-    //     response.json('no-user');
-    // } else {
-    // jwt.verify( token, secretpk, {}, ( error, decoded ) => {
-    //     if (error) throw error ;
-    //     response.json(decoded) ;
-    // })  
-    // } 
+    response.setHeader('Access-Control-Allow-Origin', 'https://paperplane-blog.onrender.com');
+    const str = request.headers.authorization;
+    let token;
+    if (str.includes('=')) {
+       token = str.replace('=', '');
+    } else {
+       token = str;
+    }
+
+    if ( token === '' ) {
+        response.json('no-user');
+    } else {
+    jwt.verify( token, secretpk, {}, ( error, decoded ) => {
+        if (error) throw error ;
+        response.json(decoded) ;
+    })  
+    } 
+    
     response.json(token)
 })
 

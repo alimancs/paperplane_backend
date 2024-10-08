58e65ab459d0ac7225ba1704c4b45b2bef3f1152
alimancs <aliman2952003@gmail.com>
2024-10-08 18:25:59 +0100

    added endpoint to send user  profile data

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ app.put('/post', async ( request, response) => {
 
     jwt.verify( token, secretpk, {}, async ( error, author ) => {
         if (error) throw error;
-        const postDoc = await postm.findById(id).populate( 'user', [ 'username']);
+        const postDoc = await postm.findById(id)
         console.log(postDoc)
         const isAuthor = JSON.stringify(postDoc.user._id) === JSON.stringify(author._id);
         if (isAuthor) {

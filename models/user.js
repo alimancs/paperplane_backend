e9d43b55c07f567ee06359a0836763961ad1433f
alimancs <aliman2952003@gmail.com>
2024-10-08 10:08:57 +0100

    added endpoint to send user  profile data

--- models/user.js
+++ models/user.js
@@ -5,7 +5,11 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
     username:{type:String, reguired:true, unique:true, min:4},
     password:{type:String, required:true},
-});
+    },
+    {
+        timestamps:true,
+    }
+);
 
 const userModel = model('user', userSchema);
 
